5cc79d6202fd59246d00d3f0aaa0561c32543ae1
Joseph Enkaoua <joseph.enkaoua@gmail.com>
2025-01-17 12:53:46 +0100

    00 01 done

--- mobileModule00/ex01/lib/main.dart
+++ mobileModule00/ex01/lib/main.dart
@@ -15,37 +15,77 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Ex00'),
+      home: const MyHomePage(title: 'Ex01'),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  String text = "A simple text";
+
+  void toggleText() {
+    setState(() {
+      text = text == "A simple text" ? "Hello world!" : "A simple text";
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
+        title: Text(widget.title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'A simple text',
+            TextCard(text: text),
+            SizedBox(
+              height: 10,
             ),
             ElevatedButton(
-              onPressed: () {
-                debugPrint('button pressed!');
-              },
+              onPressed: toggleText,
               child: Text('Click me'),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class TextCard extends StatelessWidget {
+  const TextCard({
+    super.key,
+    required this.text,
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var style = theme.textTheme.displaySmall!
+        .copyWith(color: theme.colorScheme.onPrimary);
+
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Text(
+          text,
+          style: style,
+          semanticsLabel: text,
         ),
       ),
     );
